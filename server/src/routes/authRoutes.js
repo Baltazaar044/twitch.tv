2c79e10290181a8ecbd58ceada724964e783d6f6
Baltazaar <drayden044@gmail.com>
2024-09-04 13:23:38 +0530

    first commit

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const router = express.Router();
+
+router.post("/register",(req,res)=>{
+    return res.send("This is register route");
+});
+
+router.post("/login",(req,res)=>{
+    return res.send("This is login route");
+});
+
+export default router;
